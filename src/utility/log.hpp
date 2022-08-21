8572d7b2a82593a74c46fa7cb6e1477e44d24062
Nicolas Firmo <nicolasfpbarra@gmail.com>
2022-08-21 20:27:35 -0300

    Using Windows OutputDebugString in debugLog

--- src/utility/log.hpp
+++ src/utility/log.hpp
@@ -10,7 +10,11 @@
 template <typename... T>
 constexpr void debugLog(std::string_view fmt, T&&... args) {
 	if constexpr (DEBUG)
+#if defined(PLATFORM_WINDOWS)
+		OutputDebugString(fmt::vformat(fmt, fmt::make_format_args(std::forward<T>(args)...)).c_str());
+#else
 		fmt::print(fmt::fg(fmt::color::green), fmt, std::forward<T>(args)...);
+#endif
 }
 
 #if defined(_MSC_VER)
